05239a3ac92adc53e8e2c264e4b26c6a46da3a16
swkim01 <swkim01@gmail.com>
2016-11-01 22:16:00 +0900

    Change cv codes to cv2

--- ch7/mjpgstream.py
+++ ch7/mjpgstream.py
@@ -1,10 +1,10 @@
 from bottle import route, run, get, response
-import cv2, cv
+import cv2
 
 # setup video capture
 cam = cv2.VideoCapture(0)
-cam.set(cv.CV_CAP_PROP_FRAME_WIDTH, 320)
-cam.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
 @get('/stream.mjpg')
 def do_stream():
