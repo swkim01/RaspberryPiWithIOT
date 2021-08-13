100d31646dfcad1c2e2db701f893586528d39586
swkim01 <swkim01@gmail.com>
2021-08-13 16:00:37 +0900

    update sqlitest.py

--- ch4/flask/sqlitest.py
+++ ch4/flask/sqlitest.py
@@ -1,18 +1,19 @@
 import sqlite3, sys
 
 db = sqlite3.connect("sqlitest/test.db")
-db.row_factory = sqlite3.Row
+#db.row_factory = sqlite3.Row
 cur = db.cursor()
 query = cur.execute("select * from student")
-#for row in query.fetchall():
-#    print row["id"],row["name"],row["gender"]
-while True:
-    student = cur.fetchone()
-    if not student: break
-    if sys.version_info < (3,0):
-        print(student)
-    else:
-        print([col for col in student])
+for row in query.fetchall():
+    #print(row["id"],row["name"],row["gender"])
+    print(row)
+#while True:
+#    student = cur.fetchone()
+#    if not student: break
+#    if sys.version_info < (3,0):
+#        print(student)
+#    else:
+#        print([col for col in student])
 
 cur.close()
 db.close()
