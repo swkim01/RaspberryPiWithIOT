659b84c9b3419b9d9a13f7922edc223c5144366c
swkim01 <swkim01@gmail.com>
2021-08-28 19:29:41 +0900

    update testowm.py

--- ch9/weather/testowm.py
+++ ch9/weather/testowm.py
@@ -3,22 +3,23 @@ import time
 
 # create client
 owm = pyowm.OWM('API Key')
+mgr = owm.weather_manager()
 
-lon = 129.0
 lat = 35.0
+lon = 129.0
 
 # iterate results
-o1 = owm.weather_at_coords(lat, lon)
-loc = o1.get_location()
-weat = o1.get_weather()
+o1 = mgr.weather_at_coords(lat, lon)
+loc = o1.location
+weat = o1.weather
 print("< Current Weather >")
-print('location:', str(loc.get_name()))
-print('weather:', weat.get_status(), "at", weat.get_reference_time('iso'))
-print('temperature:', weat.get_temperature(unit='celsius')['temp'], 'degree')
-wc = weat.get_weather_code()
-print('weathercode:', wc)
-#print(weat.get_weather_icon_name())
+print('location:', str(loc.name))
+print('weather:', weat.status, "at", weat.reference_time('iso'))
+print('temperature:', weat.temperature(unit='celsius')['temp'], 'degree')
 
+wc = weat.weather_code
+print('weathercode:', wc)
+#print(weat.weather_icon_name)
 if int(wc/100) == 2:
   print('Thunderstorm')
 elif int(wc/100) == 3:
@@ -31,15 +32,13 @@ elif int(wc/100) == 8:
   print('Clouds')
 elif int(wc/100) == 9:
   print('Extreme')
-
 print("")
 
-fc1 = owm.daily_forecast(str(loc.get_name()))
-fore = fc1.get_forecast()
-
-print("< Forecast Weather on", time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(fore.get_reception_time())), ">")
-#print(time.localtime(fore.get_reception_time()))
+fc1 = mgr.forecast_at_place(str(loc.name), 'daily')
+fore = fc1.forecast
+print("< Forecast Weather on", time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(fore.reception_time())), ">")
+#print(fore.reception_time(str('ico')))
 for item in fore:
     #print(item.__dict__.values())
-    lt = time.localtime(item.get_reference_time())
-    print(time.strftime("%a, %d %b %H:%M", lt), item.get_status(), item.get_weather_code())
+    lt = time.localtime(item.reference_time())
+    print(time.strftime("%a, %d %b %H:%M", lt), item.status, item.weather_code)
