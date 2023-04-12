d7568a9ff4cc46f1c5d7038295bc34f186aeea18
swkim01 <swkim01@gmail.com>
2023-04-12 14:13:05 +0900

    Update readserial.py

--- ch6/readserial.py
+++ ch6/readserial.py
@@ -1,7 +1,13 @@
 import serial
 
-con = serial.Serial('/dev/ttyAMA0', 9600)
+con = serial.Serial(port='/dev/ttyS0',
+                   baudrate=115200,
+                   parity=serial.PARITY_NONE,
+                   stopbits=serial.STOPBITS_ONE,
+                   bytesize=serial.EIGHTBITS,
+                   timeout=1)
 
 while True:
     text = con.readlines()
-    print text+'\r\n'
+    if text:
+        print(text[0].decode())
