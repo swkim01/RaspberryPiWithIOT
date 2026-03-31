13a943d4dac59f983b517b64c657d61226ccaa68
swkim01 <swkim01@gmail.com>
2026-03-31 22:06:11 +0900

    Update btn_interrupt.py

--- ch6/btn_interrupt.py
+++ ch6/btn_interrupt.py
@@ -6,14 +6,14 @@ count = 0
 def handler(channel):
     global count
     count = count + 1
-    print count
+    print(count)
 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(24, GPIO.RISING, callback=handler)
 try:  
     GPIO.wait_for_edge(23, GPIO.FALLING)
-    print "Falling edge detected."
+    print("Falling edge detected.")
 except KeyboardInterrupt:  
     GPIO.cleanup()       # clean up GPIO on CTRL_C
 GPIO.cleanup()
